cab54c8d7c942349e64976f4fe1fa4904603e89f
Christopher Ralph <chris.734c@gmail.com>
2024-09-07 18:25:03 -0400

    📦 NEW: Finished the restricted auth middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,19 +1,11 @@
 const User = require('../users/users-model');
 
-/*
-  If the user does not have a session saved in the server
-
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
-*/
 function restricted(req, res, next) {
-  // if (req.session && req.session.userId) {
-  //   next()
-  // }
-  console.log('restricted');
-  next();
+  if (req.session.user) {
+    next();
+  } else {
+    next({ status: 401, message: 'You shall not pass!' });
+  }
 }
 
 async function checkUsernameFree(req, res, next) {
@@ -33,7 +25,7 @@ async function checkUsernameExists(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username });
     if (users.length) {
-      req.user = users[0]
+      req.user = users[0];
       next();
     } else {
       next({ message: 'Invalid credentials', status: 401 });
